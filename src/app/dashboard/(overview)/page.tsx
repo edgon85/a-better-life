daf96556c4a2e8abff266301929463ecac6c18f0
Edwin Gonzalez <edgon85@gmail.com>
2024-01-22 16:35:12 -0600

    dashboard

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import { Card } from '@/components/dashboard/cards';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="">
-      <p>Hola a better life</p>
-      <p>Hola a better life 2</p>
+    <main>
+      <h1 className={` mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Suspense fallback={<CardsSkeleton />}> */}
+        <Card title="Pacientes" value={45} type="collected" />
+        <Card title="medicos" value={8} type="collected" />
+        <Card title="Clínicas" value={3} type="collected" />
+        <Card title="Citas" value={15} type="collected" />
+        {/* </Suspense> */}
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Suspense fallback={<CardsSkeleton />}> */}
+        {/* </Suspense> */}
+      </div>
+    
     </main>
   );
 }
---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,10 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+export default function AcmeLogo() {
+  return (
+    <div>
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg] text-white" />
+      <p className="text-[44px] text-white">A Better Life</p>
+    </div>
+  );
+}
